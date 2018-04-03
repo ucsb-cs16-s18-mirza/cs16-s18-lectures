6ec07034d03b329fa1c412d17d83c6f2670bcbb3
Diba Mirza <dimirza@cs.ucsb.edu>
2018-04-02 18:20:44 -0700

    lec-01 code

---
+++ lec-01/hello.cpp
@@ -0,0 +1,13 @@
+//This is a program to explore simple input output in C++
+//Diba Mirza
+
+#include <iostream> //include the modules for input and output
+using namespace std; //using functions in the standard library
+
+int main(){//Execution always starts here
+    //Write your code here
+    cout << "Hello World! "<< endl;
+    // Print "Hello world followed by a new line"
+    return 0;
+}
+
