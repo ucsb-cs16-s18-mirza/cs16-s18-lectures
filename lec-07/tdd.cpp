5c5e28bf8cb1ff7d397e9fbe4a91ede396ae162d
Diba Mirza <dimirza@cs.ucsb.edu>
2018-04-29 18:36:49 -0700

    updated lecture code

--- lec-07/tdd.cpp
+++ lec-07/tdd.cpp
@@ -1,7 +1,7 @@
 //tdd.cpp
 #include <iostream>
 #include <cstdlib>
-#include <cmath>
+
 using namespace std;
 
 void assertEqual(string expected, string actual, string message=""){
@@ -11,5 +11,5 @@ void assertEqual(string expected, string actual, string message=""){
 		cout<<"  FAILED: "<< message <<endl<<"Expected: "<<"[\n"
 		<<expected<<"\n]"<< "Actual: [\n"<<actual<<"\n]"<<endl;
 	}
-    int x = pow(2, 4);
+  
 }
--- lec-07/testDrawShapes.cpp
+++ lec-07/testDrawShapes.cpp
@@ -34,6 +34,7 @@ int main(int argc, char* argv[]){
 	}
 
 
+
 	testdrawTriangle();
 	//cout<<drawTriangle(3);
 
